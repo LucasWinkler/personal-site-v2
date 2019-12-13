b07fa12ec16ed24c4832fb89f5bd0d31ce02a3f7
Lucas <lucasj.winkler1999@gmail.com>
2019-12-12 21:48:17 -0500

    Removed all styling and content

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,11 +4,6 @@ import Nav from './Nav';
 function Header() {
   return (
     <header className='header'>
-      <h1 className='logo'>
-        <a href='index.html' className='logo__link'>
-          Lucas Winkler
-        </a>
-      </h1>
       <Nav />
     </header>
   );
