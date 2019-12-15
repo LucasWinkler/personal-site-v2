dc30fd6c823d825e2857635047ae436ad400da52
Lucas <lucasj.winkler1999@gmail.com>
2019-12-15 18:10:10 -0500

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
-import Layout from './components/Layout';
-import * as serviceWorker from './serviceWorker';
+import Layout from './components/layout/Layout';
 
 ReactDOM.render(<Layout />, document.getElementById('app'));
-serviceWorker.unregister();
