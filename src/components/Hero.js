faff04a7b6c2dde37f121bff5187bb65c00fe30e
Lucas <lucasj.winkler1999@gmail.com>
2019-12-17 23:52:07 -0500

    Styled the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,15 +6,17 @@ function Hero() {
   return (
     <section id='section-hero' className='section-hero'>
       <div className='container'>
-        <h2 className='hero-title'>{heroData.title}</h2>
-        <p className='hero-subtitle'>{heroData.subtitle}</p>
-        <button className={heroData.btnResume.style}>
-          <FontAwesomeIcon
-            icon={heroData.btnResume.icon}
-            className='btn-icon'
-          />
-          {heroData.btnResume.text}
-        </button>
+        <div className='hero-content'>
+          <h2 className='hero-title'>{heroData.title}</h2>
+          <p className='hero-subtitle'>{heroData.subtitle}</p>
+          <button className={heroData.btnResume.style}>
+            <FontAwesomeIcon
+              icon={heroData.btnResume.icon}
+              className='btn-icon'
+            />
+            {heroData.btnResume.text}
+          </button>
+        </div>
       </div>
     </section>
   );
