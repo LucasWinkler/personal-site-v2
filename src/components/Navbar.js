c8d6db8b387b2b752812f8091a5ef3d6c786df93
Lucas <lucaswinkler@gmail.com>
2019-12-19 01:15:01 -0500

    Moved the menu-socials into the main navbar menu and created a divider list-item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import navigation from '../data/navigation';
 import socials from '../data/socials';
 
 function Navbar() {
   const [scrollOffset, setScrollOffset] = useState(0);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const navClassNames = isNavOpen
+    ? 'navbar-menu navbar-menu-show'
+    : 'navbar-menu';
 
   useEffect(() => {
     setScrollOffset(document.getElementById('header').offsetHeight);
@@ -13,6 +19,9 @@ function Navbar() {
 
   return (
     <nav id='navbar' className={'navbar'}>
+      <button id='navbar-toggle' className='navbar-toggle'>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
       <AnchorLink
         href='#section-hero'
         offset={scrollOffset}
@@ -34,8 +43,23 @@ function Navbar() {
             </li>
           );
         })}
+        <li className='navbar-item-divider' />
+        {socials.map((social, i) => {
+          return (
+            <li className='navbar-item navbar-item-social' key={i}>
+              <a
+                href={social.link}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='navbar-link  navbar-link-social'
+              >
+                <FontAwesomeIcon icon={social.icon} fixedWidth />
+              </a>
+            </li>
+          );
+        })}
       </ul>
-      <ul className='navbar-menu navbar-menu-social'>
+      {/* <ul className='navbar-menu navbar-menu-social'>
         {socials.map((social, i) => {
           return (
             <li className='navbar-item' key={i}>
@@ -50,7 +74,7 @@ function Navbar() {
             </li>
           );
         })}
-      </ul>
+      </ul> */}
     </nav>
   );
 }
