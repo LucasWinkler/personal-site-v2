35b2ac4482a70af9e790dddbeb661117c5f55781
Lucas Winkler <lucaswinkler@gmail.com>
2020-01-13 15:24:04 -0500

    Styled most of the projects section

--- src/components/layout/sections/Projects.js
+++ src/components/layout/sections/Projects.js
@@ -14,7 +14,34 @@ function Projects() {
     <section id='section-projects' className='section-projects'>
       <div className='container projects-wrapper'>
         <h2 className='projects-title'>{projectData.title}</h2>
-        <div className='projects-list'></div>
+        <div className='projects-list'>
+          {projectData.projects.map((project, i) => {
+            return (
+              <div className='project' key={i}>
+                <img
+                  src={project.image} //'https://via.placeholder.com/480x320'
+                  alt='Placeholder project'
+                  className='project-image'
+                />
+                <div className='project-content'>
+                  <div className='project-title'>{project.title}</div>
+                  <div className='project-description'>
+                    {project.description}
+                  </div>
+                  <ul className='project-tech'>
+                    {project.technologies.map((tech, j) => {
+                      return (
+                        <li className='project-tech-item' key={j}>
+                          {tech}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
